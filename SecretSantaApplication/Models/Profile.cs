573c08163e2e3cf29e7ab496363fbaa762cb697c
Vitali Vishneuski <vitali.vishneuski@billwerk.com>
2020-12-03 21:51:42 +0300

    fix up

--- SecretSantaApplication/Models/Profile.cs
+++ SecretSantaApplication/Models/Profile.cs
@@ -5,7 +5,7 @@ namespace SecretSantaApplication.Models
     public class Profile
     {
         [Key] public string EmailAddress { get; set; }
-        [DataType(DataType.Password)] public string BirthDate { get; set; }
+        public string BirthDate { get; set; }
         public string LetterToSecretSanta { get; set; }
     }
 }
